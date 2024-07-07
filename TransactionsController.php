6853cb29e59d6f1a548cb620db558179e0df1cac
Giulivan Sousa <giulivan_cs@hotmail.com>
2024-07-06 23:25:56 -0300

    Feat: Criação do método para enviar os dados atualizados para a API--controller

--- TransactionsController.php
+++ TransactionsController.php
@@ -35,7 +35,6 @@ class TransactionsController
         try {
             $stmt = $this->pdo->query("SELECT * FROM transactions");
             $transactions = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            // header("Access-Control-Allow-Origin: *");
             $this->sendResponse(200, json_encode($transactions));
         } catch (PDOException $e) {
             $this->sendResponse(500, json_encode(['error' => 'Failed to retrieve transactions: ' . $e->getMessage()]));
@@ -46,12 +45,10 @@ class TransactionsController
     {
         $item = json_decode($jsonData, true);
 
-        //
         if (json_last_error() !== JSON_ERROR_NONE) {
             $this->sendResponse(400, json_encode(['error' => 'Invalid JSON data']));
             return;
         }
-        //
 
         $dataCad = $item['data_cad'];
         $type = $item['type'];
@@ -61,12 +58,10 @@ class TransactionsController
         $status = $item['status'];
         $account = $item['account'];
 
-        //
         if (!$dataCad || !$type || !$description || !$price || !$category || !$status || !$account) {
             $this->sendResponse(400, json_encode(['error' => 'Missing required fields']));
             return;
         }
-        //
 
         try {
             $stmt = $this->pdo->prepare("INSERT INTO transactions (data_cad, type, description, price, category, status, account) VALUES (:data_cad, :type, :description, :price, :category, :status, :account)");
@@ -81,19 +76,65 @@ class TransactionsController
             $this->sendResponse(201, json_encode(['message' => 'Nova transação criada com sucesso!']));
             return;
         } catch (PDOException $e) {
-            //
             error_log('Erro ao criar nova transação: ' . $e->getMessage());
-            //
             $this->sendResponse(500, json_encode(['error' => 'Erro ao criar nova transação: ' . $e->getMessage()]));
         }
 
-        $this->sendResponse(201, 'Transaction Created');
+                $this->sendResponse(201, 'Transaction Created');
     }
 
-    private function updateTransaction()
+    public function updateTransaction($jsonData)
     {
-        $data = json_decode(file_get_contents('php://input'), true);
-        $this->sendResponse(200, 'Transaction Updated');
+        $item = json_decode($jsonData, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $this->sendResponse(400, json_encode(['error' => 'Invalid JSON data']));
+            return;
+        }
+        
+        $id = $item['id'];
+        $dataCad = $item['data_cad'];
+        $type = $item['type'];
+        $description = $item['description'];
+        $price = $item['price'];
+        $category = $item['category'];
+        $status = $item['status'];
+        $account = $item['account'];
+
+        if (!$id || !$dataCad || !$type || !$description || !$price || !$category || !$status || !$account) {
+            $this->sendResponse(400, json_encode(['error' => 'Missing required fields']));
+            return;
+        }
+
+        try {
+            $stmt = $this->pdo->prepare("UPDATE transactions SET 
+            data_cad = :data_cad,
+            type = :type,
+            description = :description,
+            price = :price,
+            category = :category,
+            status = :status,
+            account = :account
+            WHERE id = :id
+            ");
+
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':data_cad', $dataCad);
+            $stmt->bindParam(':type', $type);
+            $stmt->bindParam(':description', $description);
+            $stmt->bindParam(':price', $price);
+            $stmt->bindParam(':category', $category);
+            $stmt->bindParam(':status', $status);
+            $stmt->bindParam(':account', $account);
+            $stmt->execute();
+            $this->sendResponse(201, json_encode(['message' => 'Transação atualizada com sucesso!1']));
+            return;
+        } catch (PDOException $e) {
+            error_log('Erro ao atualizar transação: ' . $e->getMessage());
+            $this->sendResponse(500, json_encode(['error' => 'Erro ao atualizar transação: ' . $e->getMessage()]));
+        }
+
+        $this->sendResponse(200, 'Transação atualizada com sucesso!2');
     }
 
     private function deleteTransaction()
